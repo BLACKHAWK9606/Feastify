a2fa2f55e36fc1540669f9b9c5895ccbb7a39e3a
BLACKHAWK9606 <jameskamaunduta@gmail.com>
2023-05-23 12:33:31 +0300

    First Commit

---
+++ checkout.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("connection/connect.php");
+include_once 'product-action.php';
+error_reporting(0);
+session_start();
+if(empty($_SESSION["user_id"]))
+{
+	header('location:login.php');
+}
+else{
+
+										  
+												foreach ($_SESSION["cart_item"] as $item)
+												{
+											
+												$item_total += ($item["price"]*$item["quantity"]);
+												
+													if($_POST['submit'])
+													{
+						
+													$SQL="insert into users_orders(u_id,title,quantity,price) values('".$_SESSION["user_id"]."','".$item["title"]."','".$item["quantity"]."','".$item["price"]."')";
+						
+														mysqli_query($db,$SQL);
+														
+														$success = "Thankyou! Your Order Placed successfully!";
+
+														
+														
+													}
+												}
+?>
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title>GOOD FOOD FOR A GOOD MOOD</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet"> </head>
+<body>
+    
+    <div class="site-wrapper">
+        <!--header starts-->
+        <header id="header" class="header-scroll top-header headrom">
+            <!-- .navbar -->
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+                            
+							<?php
+						if(empty($_SESSION["user_id"]))
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">signup</a> </li>';
+							}
+						else
+							{
+									
+									
+										echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">your orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">logout</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- /.navbar -->
+        </header>
+        <div class="page-wrapper">
+            <div class="top-links">
+                <div class="container">
+                    <ul class="row links">
+                      
+                        <li class="col-xs-12 col-sm-4 link-item"><span>1</span><a href="restaurants.php">Choose Restaurant</a></li>
+                        <li class="col-xs-12 col-sm-4 link-item "><span>2</span><a href="#">Pick Your favorite food</a></li>
+                        <li class="col-xs-12 col-sm-4 link-item active" ><span>3</span><a href="checkout.php">Order and Pay online</a></li>
+                    </ul>
+                </div>
+            </div>
+			
+                <div class="container">
+                 
+					   <span style="color:green;">
+								<?php echo $success; ?>
+										</span>
+					
+                </div>
+            
+			
+			
+				  
+            <div class="container m-t-30">
+			<form action="" method="post">
+                <div class="widget clearfix">
+                    
+                    <div class="widget-body">
+                        <form method="post" action="#">
+                            <div class="row">
+                                
+                                <div class="col-sm-12">
+                                    <div class="cart-totals margin-b-20">
+                                        <div class="cart-totals-title">
+                                            <h4>Cart Summary</h4> </div>
+                                        <div class="cart-totals-fields">
+										
+                                            <table class="table">
+											<tbody>
+                                          
+												 
+											   
+                                                    <tr>
+                                                        <td>Cart Subtotal</td>
+                                                        <td> <?php echo "Ksh".$item_total; ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Shipping &amp; Handling</td>
+                                                        <td>free shipping</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="text-color"><strong>Total</strong></td>
+                                                        <td class="text-color"><strong> <?php echo "Ksh".$item_total; ?></strong></td>
+                                                    </tr>
+                                                </tbody>
+												
+												
+												
+												
+                                            </table>
+                                        </div>
+                                    </div>
+                                    <!--cart summary-->
+                                    <div class="payment-option">
+                                        <ul class=" list-unstyled">
+                                            <li>
+                                                <label class="custom-control custom-radio  m-b-20">
+                                                    <input name="mod" id="radioStacked1" checked value="COD" type="radio" class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Payment on delivery</span>
+                                                    <br> <span>Please send your cheque to Store Name, Store Street, Store Town, Store State / County, Store Postcode.</span> </label>
+                                            </li>
+                                            <li>
+                                                <label class="custom-control custom-radio  m-b-10">
+                                                    <input name="mod"  type="radio" value="paypal" disabled class="custom-control-input"> <span class="custom-control-indicator"></span> <span class="custom-control-description">Paypal <img src="images/paypal.jpg" alt="" width="90"></span> </label>
+                                            </li>
+                                        </ul>
+                                        <p class="text-xs-center"> <input type="submit" onclick="return confirm('Are you sure?');" name="submit"  class="btn btn-outline-success btn-block" value="Order now"> </p>
+                                    </div>
+									</form>
+                                </div>
+                            </div>
+                       
+                    </div>
+                </div>
+				 </form>
+                        </div>
+            
+        </div>
+        <!-- end:page wrapper -->
+         </div>
+
+     <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
+
+<?php
+}
+?>
---
+++ restaurants.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("connection/connect.php");
+error_reporting(0);
+session_start();
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title>GOOD FOOD FOR A GOOD MOOD</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet"> </head>
+
+<body>
+           <!--header starts-->
+        <header id="header" class="header-scroll top-header headrom">
+            <!-- .navbar -->
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/food-picky-logo.png" alt=""> </a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+                            
+							<?php
+						if(empty($_SESSION["user_id"]))
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">signup</a> </li>';
+							}
+						else
+							{
+									
+									
+										echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">your orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">logout</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- /.navbar -->
+        </header>
+        <div class="page-wrapper">
+            <!-- top Links -->
+            <div class="top-links">
+                <div class="container">
+                    <ul class="row links">
+                       
+                        <li class="col-xs-12 col-sm-4 link-item active"><span>1</span><a href="restaurants.php">Choose Restaurant</a></li>
+                        <li class="col-xs-12 col-sm-4 link-item"><span>2</span><a href="#">Pick Your favorite food</a></li>
+                        <li class="col-xs-12 col-sm-4 link-item"><span>3</span><a href="#">Order and Pay online</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- end:Top links -->
+            <!-- start: Inner page hero -->
+            <div class="inner-page-hero bg-image" data-image-src="images/img/res.jpeg">
+                <div class="container"> </div>
+                <!-- end:Container -->
+            </div>
+            <div class="result-show">
+                <div class="container">
+                    <div class="row">
+                       
+                       
+                    </div>
+                </div>
+            </div>
+            <!-- //results show -->
+            <section class="restaurants-page">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-5 col-md-5 col-lg-3">
+                          
+                          
+                            <div class="widget clearfix">
+                                <!-- /widget heading -->
+                                <div class="widget-heading">
+                                    <h3 class="widget-title text-dark">
+                                 Popular tags
+                              </h3>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <div class="widget-body">
+                                    <ul class="tags">
+                                        <li> <a href="#" class="tag">
+                                    Pizza
+                                    </a> </li>
+                                        <li> <a href="#" class="tag">
+                                    Sendwich
+                                    </a> </li>
+                                        <li> <a href="#" class="tag">
+                                    Sendwich
+                                    </a> </li>
+                                        <li> <a href="#" class="tag">
+                                    Fish 
+                                    </a> </li>
+                                        <li> <a href="#" class="tag">
+                                    Desert
+                                    </a> </li>
+                                        <li> <a href="#" class="tag">
+                                    Salad
+                                    </a> </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- end:Widget -->
+                        </div>
+                        <div class="col-xs-12 col-sm-7 col-md-7 col-lg-9">
+                            <div class="bg-gray restaurant-entry">
+                                <div class="row">
+								<?php $ress= mysqli_query($db,"select * from restaurant");
+									      while($rows=mysqli_fetch_array($ress))
+										  {
+													
+						
+													 echo' <div class="col-sm-12 col-md-12 col-lg-8 text-xs-center text-sm-left">
+															<div class="entry-logo">
+																<a class="img-fluid" href="dishes.php?res_id='.$rows['rs_id'].'" > <img src="admin/Res_img/'.$rows['image'].'" alt="Food logo"></a>
+															</div>
+															<!-- end:Logo -->
+															<div class="entry-dscr">
+																<h5><a href="dishes.php?res_id='.$rows['rs_id'].'" >'.$rows['title'].'</a></h5> <span>'.$rows['address'].' <a href="#">...</a></span>
+																<ul class="list-inline">
+																	<li class="list-inline-item"><i class="fa fa-check"></i> Min Ksh 10,00</li>
+																	<li class="list-inline-item"><i class="fa fa-motorcycle"></i> 30 min</li>
+																</ul>
+															</div>
+															<!-- end:Entry description -->
+														</div>
+														
+														 <div class="col-sm-12 col-md-12 col-lg-4 text-xs-center">
+																<div class="right-content bg-white">
+																	<div class="right-review">
+																		<div class="rating-block"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star-o"></i> </div>
+																		<p> 245 Reviews</p> <a href="dishes.php?res_id='.$rows['rs_id'].'" class="btn theme-btn-dash">View Menu</a> </div>
+																</div>
+																<!-- end:right info -->
+															</div>';
+										  }
+						
+						
+						?>
+                                    
+                                </div>
+                                <!--end:row -->
+                            </div>
+                         
+                            
+                                
+                            </div>
+                          
+                          
+                           
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+  
+    
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
